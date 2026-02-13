cfc362c75576230dbcf05a4ce11668df48cb4262
yerayduran <yerayduranchaves2006@gmail.com>
2026-02-13 13:10:10 +0100

    Todo

--- Tema 6/src/Boletin1/ejercicio1y2/Alumno.java
+++ Tema 6/src/Boletin1/ejercicio1y2/Alumno.java
@@ -1,4 +1,48 @@
 package Boletin1.ejercicio1y2;
 
 public class Alumno {
+
+    private String nombre;
+    private String adn;
+
+    public Alumno(String nombre, String adn) {
+        this.nombre = nombre;
+        this.adn = adn;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAdn() {
+        return adn;
+    }
+
+    public void setAdn(String adn) {
+        this.adn = adn;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "nombre='" + nombre + '\'' +
+                ", adn='" + adn + '\'' +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Alumno alumno)) return false;
+
+        return adn.equals(alumno.adn);
+    }
+
+    @Override
+    public int hashCode() {
+        return adn.hashCode();
+    }
 }
